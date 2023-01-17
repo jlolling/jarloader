31ba785c2e40ced22e8ba0ea6ac3ee584e6af45c
Jan Lolling <jan.lolling@gmail.com>
2023-01-17 22:27:34 +0100

    More javadoc

--- src/main/java/de/jlo/talendcomp/loadjaragent/JarLoader.java
+++ src/main/java/de/jlo/talendcomp/loadjaragent/JarLoader.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2023 Jan Lolling jan.lolling@gmail.com
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.jlo.talendcomp.loadjaragent;
 
 import java.io.File;
@@ -9,6 +24,14 @@ import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.jar.JarFile;
 
+/**
+ * Loader of jar files at runtime. Use the official allowed and stable method of Instrumentation
+ * Add to the java command line: -javaagent:/your/path/to/jarloader-<version>.jar
+ * 
+ * @author jan.lolling@gmail.com
+ * Highly inspired by Chris Jennings
+ *
+ */
 public class JarLoader {
 
 	// here we keep the reference to Instrument to use it later
